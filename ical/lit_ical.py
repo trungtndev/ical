e0ccaeaa3dd835fc88bd83f61c81d2bf625d7717
anhtienpro25 <trunglh113@gmail.com>
2024-10-12 13:48:23 +0700

    fix

--- ical/lit_ical.py
+++ ical/lit_ical.py
@@ -151,6 +151,9 @@ class LitICAL(pl.LightningModule):
             sync_dist=True,
         )
 
+        val_loss = exp_loss + implicit_loss + fusion_loss
+        self.log("val_loss", val_loss, on_step=False, on_epoch=True, sync_dist=True)
+
         hyps = self.approximate_joint_search(batch.imgs, batch.mask)
 
         self.exprate_recorder([h.seq for h in hyps], batch.indices)
